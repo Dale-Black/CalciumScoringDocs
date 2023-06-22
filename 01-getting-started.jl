21fee567d3fd735712a03e69c65a35c1a29205c2
Dale Black <djblack@uci.edu>
2023-06-21 18:04:31 -0700

    update

--- 01-getting-started.jl
+++ 01-getting-started.jl
@@ -26,46 +26,109 @@ begin
 		Pkg.add("CSV")
 		Pkg.add("DataFrames")
 		Pkg.add("ImageMorphology")
-		Pkg.add("GLM")
 		Pkg.add("Statistics")
-		Pkg.add("ImageFiltering")
-		Pkg.add("Noise")
 		Pkg.add("DICOM")
 		Pkg.add(url="https://github.com/Dale-Black/CalciumScoring.jl")
 	end
 	
-	using PlutoUI, CSV, DataFrames, CairoMakie, ImageMorphology, GLM, Statistics, ImageFiltering, Noise, DICOM, CalciumScoring
+	using PlutoUI
+	using CSV
+	using DataFrames
+	using CairoMakie
+	using ImageMorphology
+	using Statistics
+	using DICOM
+	using CalciumScoring
 end
 
 # ╔═╡ d6254d97-3b3b-4b2f-b7b3-8bc58e6f34c2
 md"""
 # Overview
-This tutorial will briefly showcase different calcium scoring techniques available in CalciumScoring.jl. Specifically (1) **Agatston Scoring** which is the current gold standard, (2) **volume fraction calcium mass**, and (3) **spatially weighted calcium scoring**
+This package contains four different state of the art coronary artery calcium (CAC) quantification methods.
+1. Agatston scoring [1, 2]
+2. Volume fraction calcium mass [3]
+3. Integrated calcium mass [3, 4]
+4. Spatially weighted calcium scoring [5, 6]
 
-These techniques provide quantitative measures of calcium from non-contrast CT images. Every calcium mass technique calculates calcium mass via a calibration factor.
+This notebook will briefly showcase how to use CalciumScoring.jl to quantify calcium using the gold standard Agatston scoring technique.
 
-Calcium scoring also contains **material decomposition calcium mass**, another calcium measurement technique based on dual-energy material decomposition. [Click here](https://glassnotebook.io/repo/31) to get started using the material decomposition calcium mass technique.
+#### References
+1. [Quantification of coronary artery calcium using ultrafast computed tomography](https://doi.org/10.1016/0735-1097(90)90282-t)
+2. [Ultra-low-dose coronary artery calcium scoring using novel scoring thresholds for low tube voltage protocols—a pilot study ](https://doi.org/10.1093/ehjci/jey019)
+3. [Coronary artery calcium mass measurement based on integrated intensity and volume fraction techniques](https://doi.org/10.1101/2023.01.12.23284482)
+4. [Integrated intensity-based technique for coronary artery calcium mass measurement: A phantom study](https://doi.org/10.1002/mp.16326)
+5. [An alternative method for quantifying coronary artery calcification: the multi-ethnic study of atherosclerosis (MESA)](https://doi.org/10.1186/1471-2342-12-14)
+6. [Spatially Weighted Coronary Artery Calcium Score and Coronary Heart Disease Events in the Multi-Ethnic Study of Atherosclerosis](https://doi.org/10.1161/CIRCIMAGING.120.011981)
+
 """
 
 # ╔═╡ f3a886e7-31e8-43dd-adc7-3fb50c7d7d23
 md"""
 ## Import Packages
-First, let's import the most up-to-date version of CalciumScoring.jl, which can be found on the main/master branch of the [GitHub repository](https://github.com/Dale-Black/CalciumScoring.jl).
-
-Because we are using the unregistered version (most recent), we will need to `Pkg.add` this explicitly without using Pluto's built-in package manager. Be aware this can take a long time, especially if this is the first time being downloaded. Future work on this package will focus on improving this.
-
-To help with the formatting of this documentation, we will also add [PlutoUI.jl](https://github.com/JuliaPluto/PlutoUI.jl). We will also create some simulated coronary artery images, which will require [ImageFiltering.jl](https://github.com/JuliaImages/ImageFiltering.jl) and [Noise.jl](https://github.com/roflmaostc/Noise.jl) Lastly, to visualize the results, we're going to add [Makie.jl](https://github.com/JuliaPlots/Makie.jl).
+First, let's import the most up-to-date version of CalciumScoring.jl, which can be found on the main/master branch of the [GitHub repository](https://github.com/Dale-Black/CalciumScoring.jl). Be aware this can take a long time, especially if this is the first time being downloaded. Future work on this package will focus on improving this.
 """
 
 # ╔═╡ df5551ce-01cc-49a2-b040-e1a2b6d794a4
 TableOfContents()
 
+# ╔═╡ fac54db6-e2a1-4956-8b7c-091938e74b8e
+md"""
+## Load Simulated Phantoms
+Next, we need to load simulated images of cardiac phantoms.
+"""
+
+# ╔═╡ 29b04989-caa7-4e73-a9a1-cdad9e3509d8
+begin
+	# Validation Phantom
+	dcm_path = joinpath(pwd(), "dcms/val/52_59_73/medium/80");
+	dcms = dcmdir_parse(dcm_path)
+	dcm_array = cat([dcms[i][(0x7fe0, 0x0010)] for i in 1:length(dcms)]...; dims=3)
+
+	# Calcification Masks
+	mask_large_insert_high_density = Array(
+		CSV.read(
+			joinpath(pwd(), "dcms", "val_masks", "medium", "mask_L_LD.csv"), DataFrame;
+			header=false
+		)
+	)
+	mask_large_insert_high_density_3D = cat(
+		mask_large_insert_high_density,
+		mask_large_insert_high_density,
+		mask_large_insert_high_density; 
+		dims = 3
+	)
+end;
+
 # ╔═╡ 0188bed1-93f4-4e95-b581-0ea64847faac
 md"""
-# Visualize Simulated Phantom
-Below, in the appendix, we created a simulated phantom containing a calcification at the center. Let's visualize this image below.
+## Visualize Simulated Phantom
+We loaded a simulated phantom containing various density calcifications. Let's visualize this image below along with a mask of the large, high-density, calcification.
 
 This phantom is supposed to simulate a human thorax containing calcified coronary arteries. Since the phantom has identical geometry along each slice, the only difference along each slice will be from "quantum noise".
+"""
+
+# ╔═╡ 5c64de6c-2143-43a5-94cf-a7cdb6e4e210
+@bind z1 PlutoUI.Slider(axes(dcm_array, 3); default = 2, show_value = true)
+
+# ╔═╡ 0a3c60e5-b695-41e4-b43d-45b2b56c5b7e
+let
+	idxs = getindex.(findall(isone, mask_large_insert_high_density_3D[:, :, z1]), [1 2])
+	idxs = [t[i] for t in idxs, i in 1:length(idxs[1])]
+	
+	f = Figure(resolution = (1200, 800))
+	ax = Axis(
+		f[1, 1],
+		title = "DICOM Array"
+	)
+	heatmap!(transpose(dcm_array[:, :, z1]); colormap = :grays)
+	scatter!(idxs[:, 2], idxs[:, 1]; markersize = 4, color = (:red, 0.2))
+	hidedecorations!(ax)
+	f
+end
+
+# ╔═╡ 6bb10787-21f4-48fc-bec2-e7c23ccf7a8b
+md"""
+# Agatston Scoring
 """
 
 # ╔═╡ 87286995-8337-410d-bc12-a2c1c638aff5
@@ -87,26 +150,36 @@ Mass is then simple to obtain from the volume and known density
 
 """
 
-# ╔═╡ ae4afb28-3caf-4615-8e89-774dcef8dd7e
-ρ_high_density = 73 # mgcm-3
+# ╔═╡ f08c6b1e-0ae8-4b79-b282-d03597200a00
+begin
+	slice_thickness = dcms[1].meta[tag"Slice Thickness"] # mm
+	pixel_spacing = dcms[1][tag"Pixel Spacing"] # mm
+	voxel_spacing = [pixel_spacing..., slice_thickness]
 
-# ╔═╡ 13bf4d31-e155-41ee-81c1-55b2b6f28ecd
-md"""
-# 1. Agatston Scoring
-Calcium scoring is a technique for measuring calcium in the coronary arteries within a CT scan. Calcium scoring is traditionally associated with the Agatston scoring method [(1)](https://pubmed.ncbi.nlm.nih.gov/2407762/)
-
-This package allows users to compute the traditional Agatston score seen below.
-"""
+	num_slices = length(axes(dcm_array, 3))
+	ρ_high_density = 0.073 # mg/mm^3
+	radius = (5/2) # mm
+	calcium_volume_large = π * radius^2 * slice_thickness * num_slices
+	calcium_mass_large = calcium_volume_large * ρ_high_density
+end
 
 # ╔═╡ 31ba16d9-e51f-48ef-8731-fce208de1c30
 md"""
-## Visualize
+## Visualize Dilated Mask
 Now let's dilate the mask a little bit, to account for partial volume effect (blurring) and visualize our ROI
 """
+
+# ╔═╡ 83246419-b0e2-478c-bb0d-a7ab12c6ecd7
+@bind z2 PlutoUI.Slider(axes(dcm_array, 3); show_value = true, default = 2)
 
 # ╔═╡ 310ad8ab-838a-4080-ad09-076a56767174
 md"""
 ## Results
+"""
+
+# ╔═╡ e48e6ae3-1fc4-4fa4-8d8c-52db5dd0c19d
+md"""
+# Agaston Mass Scoring
 """
 
 # ╔═╡ 8b13e40c-b2fd-4f2d-856e-7605c5f5c0b7
@@ -116,108 +189,20 @@ We can also compute the calcium mass score, via the Agatston technique by comput
 We can assume the CT number of water is 0, and compute the correction factor by dividing the density of known calcium by the CT number of that same calcium. Let's load our simulated calibration phantom to acquire this.
 """
 
-# ╔═╡ c3837d06-7c00-49ca-8592-6b9e8e7ef528
-center_insert1, center_insert2 = 187, 318
-
-# ╔═╡ 122317ec-7c00-4b1e-9045-1314f444e56d
+# ╔═╡ b6b0e32a-8823-413d-b3f9-16334ff715cd
 md"""
-# 2. Volume Fraction Calcium Mass
-We will examine a simple yet powerful calcium quantification technique. This technique is similar to the integrated calcium mass technique, with fewer steps. All that is required is a region of interest containing all the calcium, the known HU of a specific calcium calibration rod, and the known HU of background material.
+## Load Calibration Rod
 """
 
-# ╔═╡ 017fe43a-df10-40d9-b9d1-d93d71500bd0
-md"""
-## Results
-"""
-
-# ╔═╡ a91fd5a5-cd7b-4676-a6c3-13d8c81453d9
-md"""
-# 3. Spatially Weighted Calcium Scoring
-Spatially weighted calcium scoring is another approach that attempts to improve upon the traditional Agatston approach. One way of doing this is by avoiding thresholding in favor of weighting each voxel. 
-
-Below we will see how this technique can be used in CalciumScoring.jl
-"""
-
-# ╔═╡ c624e494-ade4-4a05-98e7-f7beaeaf847f
-md"""
-#### Distribution
-First, a distribution needs to be prepared based on measurements of 100 mg/cc calibration rods. This distribution can be estimated for the sake of documentation, but this should be measured carefully in practice.
-"""
-
-# ╔═╡ a0609e90-45ea-4494-ad0a-86d339dff917
-μ, σ = 175, 20
-
-# ╔═╡ 581e9601-c169-4b2a-acec-391a75be26da
-md"""
-## Results
-Spatially weighted calcium scoring produces a score similar to Agatston scoring for high density calcifications, but more sensitive than Agatston scoring for low density calcifications. One limitation is that spatially weighted calcium scoring doesn't provide a straightforward way to convert from a spatially weighted calcium score to a physical measurement like volume score or mass score.
-"""
-
-# ╔═╡ 7484f5a6-0cbc-451f-b790-aea05813c425
-md"""
-# Appendix
-"""
-
-# ╔═╡ fac54db6-e2a1-4956-8b7c-091938e74b8e
-md"""
-#### Load Simulated Phantoms
-"""
-
-# ╔═╡ 29b04989-caa7-4e73-a9a1-cdad9e3509d8
+# ╔═╡ eb718d32-2966-4869-8550-5b98ae8dc04a
 begin
-	# Validation Phantom
-	dcm_path = joinpath(pwd(), "dcms/val/52_59_73/medium/80");
-	dcms = dcmdir_parse(dcm_path)
-	dcm_array = cat([dcms[i][(0x7fe0, 0x0010)] for i in 1:length(dcms)]...; dims=3)
-
-	# Calibration Phantom
 	dcm_path_calibration = joinpath(pwd(), "dcms/cal/200/30/80");
 	dcms_calibration = dcmdir_parse(dcm_path_calibration)
-	dcm_array_calibration = cat([dcms_calibration[i][(0x7fe0, 0x0010)] for i in 1:length(dcms_calibration)]...; dims=3)
-
-	# Calcification Masks
-	masks_medium = Dict(
-		:mask_L_HD => Array(CSV.read(joinpath(pwd(), "dcms", "val_masks", "medium", "mask_L_HD.csv"), DataFrame; header=false)),
-		:mask_M_HD => Array(CSV.read(joinpath(pwd(), "dcms", "val_masks","medium", "mask_M_HD.csv"), DataFrame; header=false)),
-		:mask_S_HD => Array(CSV.read(joinpath(pwd(), "dcms", "val_masks","medium", "mask_S_HD.csv"), DataFrame; header=false)),
-		:mask_L_MD => Array(CSV.read(joinpath(pwd(), "dcms", "val_masks","medium", "mask_L_MD.csv"), DataFrame; header=false)),
-		:mask_M_MD => Array(CSV.read(joinpath(pwd(), "dcms", "val_masks","medium", "mask_M_MD.csv"), DataFrame; header=false)),
-		:mask_S_MD => Array(CSV.read(joinpath(pwd(), "dcms", "val_masks","medium", "mask_S_MD.csv"), DataFrame; header=false)),
-		:mask_M_LD => Array(CSV.read(joinpath(pwd(), "dcms", "val_masks","medium", "mask_M_LD.csv"), DataFrame; header=false)),
-		:mask_L_LD => Array(CSV.read(joinpath(pwd(), "dcms", "val_masks","medium", "mask_L_LD.csv"), DataFrame; header=false)),
-		:mask_S_LD => Array(CSV.read(joinpath(pwd(), "dcms", "val_masks","medium", "mask_S_LD.csv"), DataFrame; header=false)),
+	dcm_array_calibration = cat(
+		[dcms_calibration[i][(0x7fe0, 0x0010)] for i in 1:length(dcms_calibration)]...;
+		dims=3
 	)
 end;
-
-# ╔═╡ 5c64de6c-2143-43a5-94cf-a7cdb6e4e210
-@bind z1 PlutoUI.Slider(axes(dcm_array, 3); default = 2, show_value = true)
-
-# ╔═╡ 9c700f7b-53b9-4f03-bef1-fef6a0e3cc68
-slice_thickness = dcms[1].meta[tag"Slice Thickness"]
-
-# ╔═╡ b25cf073-a3ab-4704-86ca-18f1ecbf6418
-number_of_slices = length(axes(dcm_array, 3))
-
-# ╔═╡ b667c99e-a012-4418-b641-b7dfce71332f
-begin
-	ground_truth_calcium_volume_large_mm3 = π * (5/2)^2 * slice_thickness * number_of_slices # mm^3
-	ground_truth_calcium_volume_large_cm3 = ground_truth_calcium_volume_large_mm3 * 1e-3
-end
-
-# ╔═╡ 93123baf-d4ad-4959-b4df-7f859654f191
-ground_truth_calcium_mass_mg_large_high_density = ground_truth_calcium_volume_large_cm3 * ρ_high_density
-
-# ╔═╡ 83246419-b0e2-478c-bb0d-a7ab12c6ecd7
-@bind z2 PlutoUI.Slider(axes(dcm_array, 3); show_value = true, default = 2)
-
-# ╔═╡ 4d8089c3-435e-4b1a-a185-bd79e1425c13
-pixel_spacing = dcms[1][tag"Pixel Spacing"]
-
-# ╔═╡ 9fc7b12c-b770-4431-bc38-0293b88a13f1
-voxel_spacing = [pixel_spacing..., slice_thickness]
-
-# ╔═╡ d37b1618-f0ed-42af-a336-8f9a341f2445
-voxel_size = voxel_spacing[1] * voxel_spacing[2] * voxel_spacing[1]
 
 # ╔═╡ f00fb7d4-5183-483a-90a2-73ff9be3a24b
 @bind z3 PlutoUI.Slider(axes(dcm_array_calibration, 3); default = 2, show_value = true)
@@ -233,6 +218,9 @@ let
 	heatmap!(transpose(dcm_array_calibration[:, :, z2]); colormap = :grays)
 	f
 end
+
+# ╔═╡ c3837d06-7c00-49ca-8592-6b9e8e7ef528
+center_insert1, center_insert2 = 187, 318
 
 # ╔═╡ 1870556b-2e19-4e64-9df1-338daae29805
 begin
@@ -251,6 +239,20 @@ end
 
 # ╔═╡ 2d3b75aa-da53-4490-a743-9a6ca405759f
 mass_calibration = ρ_calcium_200 / hu_calcium_200 * 1e-3
+
+# ╔═╡ 52c83cf7-8d82-4c84-ba57-3384ddec26f3
+md"""
+## Results
+"""
+
+# ╔═╡ cc1ad7c9-bfdf-43c3-bbf4-4e82112eb5af
+md"""
+# Next Steps
+
+The core function, `score()`, implements all four types of CAC quantification methods. Agatston scoring is the current gold standard, but recent advances in the field of medical physics have introduced better approaches (in terms of accuracy and sensitivity) to calcium mass quantification. 
+
+Check out the [Volume Fraction Calcium Mass](\volume-fraction.jl) tutorial to see how to implement one of these approaches.
+"""
 
 # ╔═╡ 982837e3-6d4c-4012-a014-886d38d19121
 md"""
@@ -310,49 +312,23 @@ function create_mask(array, mask)
 end
 
 # ╔═╡ 78d99536-b253-491e-b08d-df7cb97a2063
-"""
-    dilate_recursively(mask, n)
-
-Recursively erode a `mask`
-
-#### Inputs
-- `mask`: boolean array to erode
-- `n`: number of erosions
-
-#### Returns
-- `dilated_mask`: dilated mask with `n` erosions
-"""
 function dilate_recursively(mask, n)
     dilated_mask = copy(mask)
     for _ in 1:n
         dilated_mask = dilate(dilated_mask)
     end
     return dilated_mask
-end;
+end
 
 # ╔═╡ 51649204-97fe-460b-935e-06a29bb85d5b
 begin
-	dilated_mask_large_high_density = dilate_recursively(masks_medium[:mask_L_LD], 2)
+	dilated_mask_large_high_density = dilate_recursively(mask_large_insert_high_density, 2)
 	dilated_mask_large_high_density_3D = cat(
 		dilated_mask_large_high_density, 
 		dilated_mask_large_high_density,
 		dilated_mask_large_high_density;
 		dims=3)
 end;
-
-# ╔═╡ 0a3c60e5-b695-41e4-b43d-45b2b56c5b7e
-let
-	idxs = Tuple.(findall(isone, dilated_mask_large_high_density_3D[:, :, z1]))
-	idxs = [t[i] for t in idxs, i in 1:length(idxs[1])]
-	
-	f = Figure(resolution = (1200, 800))
-	ax = Axis(
-		f[1, 1],
-		title = "DICOM Array"
-	)
-	heatmap!(transpose(dcm_array[:, :, z1]); colormap = :grays)
-	f
-end
 
 # ╔═╡ 41340d94-d8e6-48cd-879f-707017969911
 pure_calcification_large_high_density = create_mask(dcm_array, dilated_mask_large_high_density_3D);
@@ -367,21 +343,12 @@ _, _, mass_score = score(pure_calcification_large_high_density, voxel_spacing, m
 md"""
 Let's compare that with the ground truth mass
 
-We see that the ground truth mass = $(ground_truth_calcium_mass_mg_large_high_density) is close to the calculated mass = $(mass_score)
-"""
-
-# ╔═╡ 15a6ff92-a3c1-4ff1-bc4b-674adbc53c0d
-swcs = score(pure_calcification_large_high_density, μ, σ, SpatiallyWeighted())
-
-# ╔═╡ 1e763d9c-44e8-4614-94fb-be9b65f100a9
-md"""
-We see that the spatially weighted calcium score (`swcs` = $(swcs)) is very similar to the Agatston score (`agatston_score` = $(agatston_score))
+We see that the ground truth mass = $(round.(calcium_mass_large, digits = 3)) is close to the calculated mass = $(round.(mass_score, digits = 3))
 """
 
 # ╔═╡ 907fe9d2-a67e-4716-9c73-0ce429ae2b4f
 let
-	idxs = Tuple.(findall(isone, dilated_mask_large_high_density_3D[:, :, z2]))
-	idxs = [t[i] for t in idxs, i in 1:length(idxs[1])]
+	idxs = getindex.(findall(isone, dilated_mask_large_high_density_3D[:, :, z2]), [1 2])
 	
 	f = Figure(resolution = (1200, 800))
 	
@@ -400,83 +367,48 @@ let
 	f
 end
 
-# ╔═╡ 7635fbef-61fa-4311-bda4-f7f384a4832d
-volume_fraction_mass = score(dcm_array[dilated_mask_large_high_density_3D], hu_calcium_200, 30, voxel_size, ρ_calcium_200, VolumeFraction())
-
-# ╔═╡ 11c49d54-2088-4f6c-83a1-129960889492
-md"""
-Let's compare that with the ground truth mass
-
-We see that the ground truth mass = $(ground_truth_calcium_mass_mg_large_high_density) is close to the calculated mass = $(volume_fraction_mass)
-"""
-
 # ╔═╡ 7778b027-68be-46c7-a8f3-31413fa5554f
-"""
-    erode_recursively(mask, n)
-
-Recursively erode a `mask`
-
-#### Inputs
-- `mask`: boolean array to erode
-- `n`: number of erosions
-
-#### Returns
-- `eroded_mask`: eroded mask with `n` erosions
-"""
 function erode_recursively(mask, n)
     eroded_mask = copy(mask)
     for _ in 1:n
         eroded_mask = erode(eroded_mask)
     end
     return eroded_mask
-end;
+end
 
 # ╔═╡ Cell order:
 # ╟─d6254d97-3b3b-4b2f-b7b3-8bc58e6f34c2
 # ╟─f3a886e7-31e8-43dd-adc7-3fb50c7d7d23
 # ╠═9419f973-985f-41af-b6a4-bbd6af9ea2a4
 # ╠═df5551ce-01cc-49a2-b040-e1a2b6d794a4
+# ╟─fac54db6-e2a1-4956-8b7c-091938e74b8e
+# ╠═29b04989-caa7-4e73-a9a1-cdad9e3509d8
 # ╟─0188bed1-93f4-4e95-b581-0ea64847faac
 # ╟─5c64de6c-2143-43a5-94cf-a7cdb6e4e210
 # ╟─0a3c60e5-b695-41e4-b43d-45b2b56c5b7e
+# ╟─6bb10787-21f4-48fc-bec2-e7c23ccf7a8b
 # ╟─87286995-8337-410d-bc12-a2c1c638aff5
-# ╠═9c700f7b-53b9-4f03-bef1-fef6a0e3cc68
-# ╠═b25cf073-a3ab-4704-86ca-18f1ecbf6418
-# ╠═ae4afb28-3caf-4615-8e89-774dcef8dd7e
-# ╠═b667c99e-a012-4418-b641-b7dfce71332f
-# ╠═93123baf-d4ad-4959-b4df-7f859654f191
-# ╟─13bf4d31-e155-41ee-81c1-55b2b6f28ecd
+# ╠═f08c6b1e-0ae8-4b79-b282-d03597200a00
 # ╟─31ba16d9-e51f-48ef-8731-fce208de1c30
 # ╠═51649204-97fe-460b-935e-06a29bb85d5b
 # ╠═41340d94-d8e6-48cd-879f-707017969911
 # ╟─83246419-b0e2-478c-bb0d-a7ab12c6ecd7
 # ╟─907fe9d2-a67e-4716-9c73-0ce429ae2b4f
 # ╟─310ad8ab-838a-4080-ad09-076a56767174
-# ╠═4d8089c3-435e-4b1a-a185-bd79e1425c13
-# ╠═9fc7b12c-b770-4431-bc38-0293b88a13f1
 # ╠═7e4c3269-2a2b-4b15-a373-4daf815a72ef
+# ╟─e48e6ae3-1fc4-4fa4-8d8c-52db5dd0c19d
 # ╟─8b13e40c-b2fd-4f2d-856e-7605c5f5c0b7
+# ╟─b6b0e32a-8823-413d-b3f9-16334ff715cd
+# ╠═eb718d32-2966-4869-8550-5b98ae8dc04a
 # ╟─f00fb7d4-5183-483a-90a2-73ff9be3a24b
 # ╟─efd98ea7-b86a-41e1-9eca-6675dffc7edd
 # ╠═c3837d06-7c00-49ca-8592-6b9e8e7ef528
 # ╠═1870556b-2e19-4e64-9df1-338daae29805
 # ╠═2d3b75aa-da53-4490-a743-9a6ca405759f
+# ╟─52c83cf7-8d82-4c84-ba57-3384ddec26f3
 # ╠═ade72d79-4082-4c4e-b14a-744651731d9d
 # ╟─320405f5-4feb-4c61-98f4-ff13643490ae
-# ╟─122317ec-7c00-4b1e-9045-1314f444e56d
-# ╟─017fe43a-df10-40d9-b9d1-d93d71500bd0
-# ╠═d37b1618-f0ed-42af-a336-8f9a341f2445
-# ╠═7635fbef-61fa-4311-bda4-f7f384a4832d
-# ╟─11c49d54-2088-4f6c-83a1-129960889492
-# ╟─a91fd5a5-cd7b-4676-a6c3-13d8c81453d9
-# ╟─c624e494-ade4-4a05-98e7-f7beaeaf847f
-# ╠═a0609e90-45ea-4494-ad0a-86d339dff917
-# ╟─581e9601-c169-4b2a-acec-391a75be26da
-# ╠═15a6ff92-a3c1-4ff1-bc4b-674adbc53c0d
-# ╟─1e763d9c-44e8-4614-94fb-be9b65f100a9
-# ╟─7484f5a6-0cbc-451f-b790-aea05813c425
-# ╟─fac54db6-e2a1-4956-8b7c-091938e74b8e
-# ╠═29b04989-caa7-4e73-a9a1-cdad9e3509d8
+# ╟─cc1ad7c9-bfdf-43c3-bbf4-4e82112eb5af
 # ╟─982837e3-6d4c-4012-a014-886d38d19121
 # ╟─107e9699-6baa-4978-a68f-9fc22d76d6a1
 # ╟─ac9da8e2-aee9-4c79-8e7d-f35eb7e674f2
---
+++ 02-volume-fraction.jl
@@ -0,0 +1,241 @@
+### A Pluto.jl notebook ###
+# v0.19.26
+
+using Markdown
+using InteractiveUtils
+
+# This Pluto notebook uses @bind for interactivity. When running this notebook outside of Pluto, the following 'mock version' of @bind gives bound variables a default value (instead of an error).
+macro bind(def, element)
+    quote
+        local iv = try Base.loaded_modules[Base.PkgId(Base.UUID("6e696c72-6542-2067-7265-42206c756150"), "AbstractPlutoDingetjes")].Bonds.initial_value catch; b -> missing; end
+        local el = $(esc(element))
+        global $(esc(def)) = Core.applicable(Base.get, el) ? Base.get(el) : iv(el)
+        el
+    end
+end
+
+# ╔═╡ 359ca243-8ad1-4728-97fe-35022314dae3
+# ╠═╡ show_logs = false
+begin
+	let
+		using Pkg
+		Pkg.activate(temp = true)
+		Pkg.Registry.update()
+		Pkg.add("PlutoUI")
+		Pkg.add("CairoMakie")
+		Pkg.add("CSV")
+		Pkg.add("DataFrames")
+		Pkg.add("ImageMorphology")
+		Pkg.add("Statistics")
+		Pkg.add("DICOM")
+		Pkg.add("Unitful")
+		Pkg.add(url="https://github.com/Dale-Black/CalciumScoring.jl")
+	end
+	
+	using PlutoUI
+	using CSV
+	using DataFrames
+	using CairoMakie
+	using ImageMorphology
+	using Statistics
+	using DICOM
+	using Unitful: mm, mg, ustrip
+	using CalciumScoring
+
+	TableOfContents()
+end
+
+# ╔═╡ f5fd4bdc-1093-11ee-35c9-db1ff5709044
+md"""
+# Overview
+[Previously](01-getting-started.jl), we introduced the CalciumScoring.jl package and specificall showcased the Agatston scoring method. In this notebook we will examine how to use the Volume Fraction Calium Mass method. To understand the theory behind this technique, please see [Coronary artery calcium mass measurement based on integrated intensity and volume fraction techniques](https://doi.org/10.1101/2023.01.12.23284482)
+"""
+
+# ╔═╡ d53b940a-978a-4497-a274-85cc31d6d12a
+md"""
+## Preparation
+Let's quickly (1) import all the neccessary packages, (2) load the same simulated phantom images and masks from before, and (3) calculate the ground truth calcium mass (using Unitful to keep track of units).
+"""
+
+# ╔═╡ 3ce4d714-6852-4f73-b5c2-0d67b6405d9a
+md"""
+# Volume Fraction Calcium Mass
+This calcium quantification technique does not require any intensity-based threhsolding. To calculate the calclium contained within a region of interest (ROI), the mean intensity of the background material is needed.
+
+To do this, we will dilate the dilated mask again and then subtract.
+"""
+
+# ╔═╡ 61a0988b-6776-4620-83a5-4f274aa73952
+md"""
+## Background calculation
+"""
+
+# ╔═╡ 9d266d02-cd20-4db1-83b6-d18ca5a68006
+md"""
+## Visualize Insert and Background Masks
+"""
+
+# ╔═╡ 7f612715-b4fc-4de3-be11-ddc6c2ed72c1
+md"""
+## Results
+"""
+
+# ╔═╡ fec583af-b4a0-452d-9ac5-5b953caef70c
+md"""
+# Next Steps
+
+Check out the [Integrated Calcium Mass](\03-integated.jl) tutorial to see how to implement one of these approaches.
+"""
+
+# ╔═╡ c916adfb-9336-42cc-ae6a-9b1f92e894a2
+md"""
+#### Helper Functions
+"""
+
+# ╔═╡ 8b9a3224-5306-4648-b51e-5792ce9e0e55
+function dilate_recursively(mask, n)
+    dilated_mask = copy(mask)
+    for _ in 1:n
+        dilated_mask = dilate(dilated_mask)
+    end
+    return dilated_mask
+end
+
+# ╔═╡ a084d53a-cb7f-4d65-a716-7d35a4b47b6f
+begin
+	# Validation Phantom
+	dcm_path = joinpath(pwd(), "dcms/val/52_59_73/medium/80");
+	dcms = dcmdir_parse(dcm_path)
+	dcm_array = cat([dcms[i][(0x7fe0, 0x0010)] for i in 1:length(dcms)]...; dims=3)
+
+	# Calcification Masks
+	mask_large_insert_high_density = Array(
+		CSV.read(
+			joinpath(pwd(), "dcms", "val_masks", "medium", "mask_L_LD.csv"), DataFrame;
+			header=false
+		)
+	)
+	mask_large_insert_high_density_3D = cat(
+		mask_large_insert_high_density,
+		mask_large_insert_high_density,
+		mask_large_insert_high_density; 
+		dims = 3
+	)
+
+	# Calibration Rod
+	dcm_path_calibration = joinpath(pwd(), "dcms/cal/200/30/80");
+	dcms_calibration = dcmdir_parse(dcm_path_calibration)
+	dcm_array_calibration = cat(
+		[dcms_calibration[i][(0x7fe0, 0x0010)] for i in 1:length(dcms_calibration)]...;
+		dims=3
+	)
+
+	center_insert1, center_insert2 = 187, 318
+	calibration_rod = zeros(25, 25, size(dcm_array_calibration, 3))
+				
+	for z in axes(dcm_array_calibration, 3)
+		rows, cols, depth = size(dcm_array_calibration)
+		half_row, half_col = center_insert1, center_insert2
+		offset = 12
+		row_range = half_row-offset:half_row+offset
+		col_range = half_col-offset:half_col+offset	
+		calibration_rod[:, :, z] .= dcm_array_calibration[row_range, col_range, z];
+	end
+	hu_calcium_200, ρ_calcium_200 = mean(calibration_rod), 0.200mg/mm^3
+
+
+	# Dilate mask
+	dilated_mask_large_high_density = dilate_recursively(mask_large_insert_high_density, 2)
+	dilated_mask_large_high_density_3D = cat(
+		dilated_mask_large_high_density, 
+		dilated_mask_large_high_density,
+		dilated_mask_large_high_density;
+		dims=3)
+end;
+
+# ╔═╡ 70f4abc8-36a3-4f2c-9e54-21562c87d16f
+begin
+	slice_thickness = dcms[1].meta[tag"Slice Thickness"]mm
+	pixel_spacing = dcms[1][tag"Pixel Spacing"]mm
+	voxel_spacing = [pixel_spacing..., slice_thickness]
+
+	num_slices = length(axes(dcm_array, 3))
+	ρ_high_density = 0.073mg/mm^3
+	radius = (5/2)mm
+	calcium_volume_large = π * radius^2 * slice_thickness * num_slices
+	calcium_mass_large = calcium_volume_large * ρ_high_density
+end
+
+# ╔═╡ 3abad7b7-37b4-415e-b3fe-34428d2b8d17
+voxel_size = voxel_spacing[1] * voxel_spacing[2] * voxel_spacing[1]
+
+# ╔═╡ a0a67d7a-183f-464e-b21a-032d32de09bb
+@bind z1 PlutoUI.Slider(axes(dcm_array, 3); default = 2, show_value = true)
+
+# ╔═╡ e43b6288-08f8-463a-8324-89eb32bcdaba
+begin
+	_background_mask_3D = dilate_recursively(dilated_mask_large_high_density_3D, 3)
+	background_mask_3D = Bool.(_background_mask_3D - dilated_mask_large_high_density_3D)
+end;
+
+# ╔═╡ c37667c7-5074-43a1-a75d-8e44884c7160
+let
+	idxs = getindex.(findall(isone, dilated_mask_large_high_density_3D[:, :, z1]), [1 2])
+	idxs2 = getindex.(findall(isone, background_mask_3D[:, :, z1]), [1 2])
+	
+	f = Figure(resolution = (1200, 800))
+	ax = Axis(
+		f[1, 1],
+		title = "DICOM Array"
+	)
+	heatmap!(transpose(dcm_array[:, :, z1]); colormap = :grays)
+	scatter!(idxs[:, 2], idxs[:, 1]; markersize = 4, color = (:red, 0.2))
+	hidedecorations!(ax)
+	scatter!(idxs2[:, 2], idxs2[:, 1]; markersize = 4, color = (:blue, 0.2))
+	hidedecorations!(ax)
+	f
+end
+
+# ╔═╡ 539dfd12-5b93-44d4-b883-0d08ebadbe55
+hu_background = mean(dcm_array[background_mask_3D])
+
+# ╔═╡ 5be9d073-e8c5-44d6-8982-639cbe8c3c6a
+volume_fraction_mass = score(dcm_array[dilated_mask_large_high_density_3D], hu_calcium_200, hu_background, voxel_size, ρ_calcium_200, VolumeFraction())
+
+# ╔═╡ 050f4296-e9ea-4bc7-ae28-b98f2a6c0211
+md"""
+Let's compare that with the ground truth mass
+
+We see that the ground truth mass = $(round.(mg, calcium_mass_large; digits = 3)) is close to the calculated mass = $(round.(mg, volume_fraction_mass; digits = 3))
+"""
+
+# ╔═╡ e05ae3d3-5a7d-4288-a3a1-703fadf86ef5
+function erode_recursively(mask, n)
+    eroded_mask = copy(mask)
+    for _ in 1:n
+        eroded_mask = erode(eroded_mask)
+    end
+    return eroded_mask
+end
+
+# ╔═╡ Cell order:
+# ╟─f5fd4bdc-1093-11ee-35c9-db1ff5709044
+# ╟─d53b940a-978a-4497-a274-85cc31d6d12a
+# ╠═359ca243-8ad1-4728-97fe-35022314dae3
+# ╠═a084d53a-cb7f-4d65-a716-7d35a4b47b6f
+# ╠═70f4abc8-36a3-4f2c-9e54-21562c87d16f
+# ╟─3ce4d714-6852-4f73-b5c2-0d67b6405d9a
+# ╟─61a0988b-6776-4620-83a5-4f274aa73952
+# ╠═e43b6288-08f8-463a-8324-89eb32bcdaba
+# ╟─9d266d02-cd20-4db1-83b6-d18ca5a68006
+# ╟─a0a67d7a-183f-464e-b21a-032d32de09bb
+# ╟─c37667c7-5074-43a1-a75d-8e44884c7160
+# ╠═539dfd12-5b93-44d4-b883-0d08ebadbe55
+# ╟─7f612715-b4fc-4de3-be11-ddc6c2ed72c1
+# ╠═3abad7b7-37b4-415e-b3fe-34428d2b8d17
+# ╠═5be9d073-e8c5-44d6-8982-639cbe8c3c6a
+# ╟─050f4296-e9ea-4bc7-ae28-b98f2a6c0211
+# ╟─fec583af-b4a0-452d-9ac5-5b953caef70c
+# ╟─c916adfb-9336-42cc-ae6a-9b1f92e894a2
+# ╟─8b9a3224-5306-4648-b51e-5792ce9e0e55
+# ╟─e05ae3d3-5a7d-4288-a3a1-703fadf86ef5
